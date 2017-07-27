13e1a76a8875fe25490367845a54069cb3aaf52a
S_YETTI <abdelnassam@gmail.com>
2017-07-27 12:19:54 -0400

    Definetly fixed

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,7 +1,7 @@
 extends Node2D
-onready var enemy = preload("res://Enemy.tscn")
-onready var spawn_ = get_node("Position2D")
-onready var spawn_place = get_node("Position2D").get_global_pos()
+#onready var enemy = preload("res://Enemy.tscn")
+#nready var spawn_ = get_node("Position2D")
+#onready var spawn_place = get_node("Position2D").get_global_pos()
 
 func _ready():
 	pass
