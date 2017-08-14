a124ead4611b8a060d17f0c198238dc90c586d58
S_YETTI <abdelnassam@gmail.com>
2017-08-14 17:26:58 -0400

    VERY EARLY ALPHA RELEASE!!!!
    Enemy works but theres only one  and you cant really take damage yet

--- Enemy.gd
+++ Enemy.gd
@@ -1,22 +1,10 @@
 extends KinematicBody2D
 var health = 100
 
-onready var area = get_node("Area2D")
-
-func _ready():
-	pass
-
-
-
-
-	
-	
-
 func _on_Area2D_body_enter( body ):
-	
 	if body.is_in_group("Bullet"):
 		health -= 25
 		body.queue_free()
-		print("dane")
 	if health <= 0:
 		queue_free()
+		get_tree().get_root().get_node("CRT/Viewport/Main/Enemy_timer").start()
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,10 +1,18 @@
 extends Node2D
-#onready var enemy = preload("res://Enemy.tscn")
-#nready var spawn_ = get_node("Position2D")
-#onready var spawn_place = get_node("Position2D").get_global_pos()
+var Enemy = preload("res://Enemy.tscn")
+onready var spawn_ = get_node("Spaw_enemy")
+onready var spawn_place = spawn_.get_global_pos()
 
 func _ready():
 	pass
 	
+
 	
 
+
+
+func _on_Enemy_timer_timeout():
+	var Enemyy = Enemy.instance()
+	get_parent().add_child(Enemyy)
+	Enemyy.set_global_pos(spawn_place)
+	
